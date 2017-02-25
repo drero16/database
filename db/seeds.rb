216051b1ef8cd7c4ee10a13eddbd54c4dc84cce6
drero16 <drero16@hotmail.com>
2017-02-25 03:19:50 -0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+r1 = Role.create({name: "Admin", description: "Administrador. "})
+r2 = Role.create({name: "Member", description: "Miembro"})
+r3 = Role.create({name: "Guest", description: "No registrado"})
+
+u1 = User.create({name: "Carla", email: "carla@carla.cl", password: "123456", password_confirmation: "123456", role_id: r1.id})
