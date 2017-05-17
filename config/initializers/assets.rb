946a3d7edb36b8f55978f1d9356eda65e67cade3
drero16 <drero16@hotmail.com>
2017-05-17 03:12:27 -0400

    Show animal con galeria

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.configure do
-  config.assets.precompile += %w[ram.scss serviceworker.js manifest.json form-elements.css style.scss styleLogin.css jquery.backstretch.js main.scss centered-columns.scss  ejemploPerdido.scss gmaps.js jquery.backstretch.json auth.scss info.scss]
+  config.assets.precompile += %w[photoswipe.scss default-skin.scss photoswipe.js photoswipe-ui-default.js ram.scss serviceworker.js manifest.json form-elements.css style.scss styleLogin.css jquery.backstretch.js main.scss centered-columns.scss  ejemploPerdido.scss gmaps.js jquery.backstretch.json auth.scss info.scss]
 end
 
 %w( adoptions risks animals comments devices events images information notifications pets races roles users welcome  ).each do |controller|
