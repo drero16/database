3ff1227ccc7c0c73926f7a76974daacea102b7cb
drero16 <drero16@hotmail.com>
2017-07-01 03:09:29 -0400

    Casi terminado, probando notificaciones para servidor 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,7 +75,14 @@ namespace :deploy do
 
   desc "Reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+     puts "\n=== Seeding Database ===\n"
+    on primary :db do
+      within current_path do
+    with rails_env: fetch(:stage) do
+      execute :rake, 'db:seed'
+    end
+  end
+ end
   end
 
   before :starting,     :check_revision
